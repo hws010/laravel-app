f357b8ff130afe866acc0081de231ee2fb9b7120
hws <mxm0028@gmail.com>
2026-06-08 14:04:09 +0300

    completed 19%: stopped after ORM

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
+use App\Models\Idea;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home');
-Route::view('/about', 'about');
-Route::view('/contact', 'contact');
+Route::get('/', function(){
+    $state = request('state');
+    $ideas = Idea::query()
+    ->when($state, function($query, $state){
+        $query->where('state', $state);
+    })->get();
+
+    return view('home', [
+        'ideas' => $ideas
+    ]);
+});
+
+Route::post('/', function(){
+    $idea = request()->idea;
+
+    Idea::create([
+        'descreption' => $idea,
+        'state' => 'pendding',
+    ]);
+
+    return redirect('/');
+})->name('about');
+
+Route::get('/delete-ideas', function(){
+    session()->forget('ideas');
+    return redirect('/');
+});
